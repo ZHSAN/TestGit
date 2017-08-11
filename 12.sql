054ef39cec31e134624d598dd3ef53049c11ea62
Zhang San <1165926239@qq.com>
2017-08-11 17:09:18 +0800

    test add file

---
+++ 12.sql
@@ -0,0 +1,61 @@
+﻿prompt Importing table Sys_Region...
+set feedback off
+set define off
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('402824815dcb486f015dcb63f4020005', '同步测试', '123', '402824815d7278c6015d7284064b0002', null, '3', '同步', '同步测试修的萨克', '1', '2');
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213babae37013babc027250006', '储运部', '144', '0', null, '1', '运输', '测试修改', '1', '111');
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213c2d74f8013c36e7eb640017', '治安保卫部', '154', '0', null, null, null, '测试修改', '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213c2d74f8013c36e927cb0018', '办公室', null, '0', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213d05733c013d1ea764ec0d67', '财务处', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213f641ec2013f6423b7ed0008', '纪安监察处', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213c83b901013c84af214601d3', '西北燃气运输公司', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213babae37013babc0b3e40007', '西北销售', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213c2d74f8013c36e7532c0016', '生产运行处', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213babae37013babc0e7290008', '信息管理部', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213bd4f051013bd4f47c500004', '昆仑燃气公司', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213d66cdb6013d6c05153b02c2', '电仪部', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213bd4f051013bd4f43bad0003', '西北化工', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213babae37013babbfb6890005', '营销调运部', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213bd4f051013bd4f3e2c00002', '中燃油公司', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881213c2d74f8013c36e950130019', '规划计划处', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('8a4881214327efb4014341a0ce7306ad', '中国石油天然气运输公司甘肃化工配送分公司', null, '-1', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('402824815d7278c6015d7284064b0002', '二哈军团', null, '8a4881213babae37013babc027250006', null, null, null, null, '1', null);
+
+insert into Sys_Region (REGION_KEY, REGION_NAME, REGION_CODE, PARENT_REGION_CODE, IS_END, REGION_LEVEL, REGION_SHOT_NAME, REGION_REMARK, IS_FLAG, REGION_SEQ_NUM)
+values ('402824815d78d088015d78e015a80003', '保安大队', '144', '8a4881213c2d74f8013c36e7eb640017', null, null, null, '测试', '0', null);
+
+prompt Done.
